52d79776abec978a83df4663b9dcfd20233fb940
Warren Price <warpri81@gmail.com>
2013-04-27 15:28:54 -0500

    refactored test.py to use unittest library ===== - refactored test.py to use unittest library and added tests for adding   users and groups

--- test.py
+++ test.py
@@ -1,37 +1,30 @@
-import sys
 import os
+import unittest
 
 import pyFileZilla
 
-def print_usage():
-    print "Usage: %s [FileZilla path]" % (sys.argv[0])
+
+CONFIG_FILE_NAME = "temp.xml"
+FILEZILLA_SERVER_XML = r"""<?xml version="1.0"?><FileZillaServer><Settings></Settings><Groups></Groups><Users></Users></FileZillaServer>"""
 
 
-def test_groups(settings):
-    print "Testing groups"
-    print "=============="
-    try:
-        groups = settings.groups
-    except Exception, err:
-        print "Could not load groups: %s" % err
+class TestAddingData(unittest.TestCase):
+    def testAddUser(self):
+        settings = pyFileZilla.ftpSettings(CONFIG_FILE_NAME, None)
+        user = settings.addUser('test')
+        self.assertIsInstance(user, pyFileZilla.ftpUser)
+        self.assertRaises(pyFileZilla.UserExistsError, settings.addUser, 'TEST')
 
+    def testAddGroup(self):
+        settings = pyFileZilla.ftpSettings(CONFIG_FILE_NAME, None)
+        group = settings.addGroup('test')
+        self.assertIsInstance(group, pyFileZilla.ftpGroup)
+        self.assertRaises(pyFileZilla.GroupExistsError, settings.addGroup, 'TEST')
 
+    
 if __name__ == '__main__':
-    print "Testing pyFileZilla version %s" % pyFileZilla.VERSION
-    print "==============================="
-    if (len(sys.argv) != 2):
-        print_usage()
-        sys.exit(1)
-    filezilla_path = sys.argv[1]
-    if not os.path.isdir(filezilla_path):
-        print "Could not find FileZilla path: %s" % filezilla_path
-        print_usage()
-        sys.exit(1)
-    try:
-        settings = pyFileZilla.ftpSettings(os.path.join(filezilla_path, 'FileZilla Server.xml'), os.path.join(filezilla_path, 'FileZilla server.exe'))
-        print "ftpSettings loaded"
-        print "=================="
-        test_groups(settings)
-    except Exception, err:
-        print "Could not load ftpSettings object %s" % err
-        sys.exit(2)
+    config_file = file(CONFIG_FILE_NAME, 'wb')
+    config_file.write(FILEZILLA_SERVER_XML)
+    config_file.close()
+    unittest.main()
+    os.remove(CONFIG_FILE_NAME)
