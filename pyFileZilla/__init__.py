7a9f117618e8f050b56a0fe99ff74127ad0cbf42
Warren Price <warpri81@gmail.com>
2013-04-27 15:26:44 -0500

    added UserExistsError and GroupExistsError to pyFileZilla package ===== - added imports for UserExistsError and GroupExistsError to   pyFileZilla/__init__.py

--- pyFileZilla/__init__.py
+++ pyFileZilla/__init__.py
@@ -4,4 +4,5 @@ from ftpSettings import \
     ftpSettings, \
     ftpGroup, ftpUser, \
     ftpPermission, ftpSpeedLimit, \
+    UserExistsError, GroupExistsError, \
     SPEED_DEFAULT, SPEED_UNLIMITED, SPEED_CONSTANT, SPEED_RULES
